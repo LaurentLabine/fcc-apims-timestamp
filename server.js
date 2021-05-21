360b801606f46f5b57e8b318751c4f92df8745df
Laurent Labine <laurentlabine@gmail.com>
2021-05-21 19:48:23 +0200

    Removed debug console.log

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // server.js
 // where your node app starts
+//webpack HOT configuration : https://webpack.js.org/guides/hot-module-replacement/
 
 // init project
 require('dotenv').config();
@@ -7,12 +8,12 @@ var express = require('express');
 var app = express();
 
 
-Date.prototype.getUnixTime = function() { return this.getTime()/1000|0 };
+Date.prototype.getUnixTime = function () { return this.getTime() / 1000 | 0 };
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -25,47 +26,43 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-app.get("/api/timestamp/:date?", function(req, res) {
+app.get("/api/timestamp/:date?", function (req, res) {
 
-const param = req.params.date;
-let utcMilliSeconds;
-let date = new Date(param);
-let response = {};
-console.log(typeof(param))
+  const param = req.params.date;
+  let utcMilliSeconds;
+  let date = new Date(param);
+  let response = {};
 
-if(!param){
-  console.log("undefined")
+  if (!param) {
 
-  var currentdate = new Date(); 
-var datetime = currentdate.getFullYear() + "/"
-                + (currentdate.getMonth()+1)  + "/"
-                + currentdate.getDate() + " "
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-                console.log(datetime)
+    var currentdate = new Date();
+    var datetime = currentdate.getFullYear() + "/"
+      + (currentdate.getMonth() + 1) + "/"
+      + currentdate.getDate() + " "
+      + currentdate.getHours() + ":"
+      + currentdate.getMinutes() + ":"
+      + currentdate.getSeconds();
 
-  date = new Date(datetime)
-  utcMilliSeconds = date.getUnixTime()*1000
+    date = new Date(datetime)
+    utcMilliSeconds = date.getUnixTime() * 1000
 
-} else if(!isNaN(param)){//If number
-  console.log("Number")
+  } else if (!isNaN(param)) {//If number
     date = new Date(0)
     date.setUTCMilliseconds(param)
-    utcMilliSeconds = date.getUnixTime()*1000
-    console.log(date)
-} else if(!isNaN(date.getTime())){//If valid date
+    utcMilliSeconds = date.getUnixTime() * 1000
+  } else if (!isNaN(date.getTime())) {//If valid date
     date = new Date(param);
-    utcMilliSeconds = date.getUnixTime()*1000
-}else return (res.json({ error : "Invalid Date"})); //Invalid Format
+    utcMilliSeconds = date.getUnixTime() * 1000
+  } else return (res.json({ error: "Invalid Date" })); //Invalid Format
 
   return (res.json(
-      { unix : utcMilliSeconds, 
-        utc: date.toUTCString()
-  }))
+    {
+      unix: utcMilliSeconds,
+      utc: date.toUTCString()
+    }))
 });
 
 // listen for requests :)
